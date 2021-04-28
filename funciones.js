4feb20771444623278b2f292f8e904a992c89cf8
Diego Rodriguez <drodriguez@codigodelsur.com>
2021-04-27 21:38:04 -0300

    tercera clase

---
+++ funciones.js
@@ -0,0 +1,66 @@
+
+/* function calcularArea(lado1, lado2){
+
+
+    alert('lado1----'+lado1)
+    alert('lado2---'+lado2)
+
+    var area = lado1 * lado2;
+
+    return area;
+}
+
+var lado1 = 100;
+var lado2 = 200;
+
+calcularArea(lado2, lado1);
+
+
+function equipoDeFutbol(nombre1, nombre2, nombre3){
+
+    alert('El nombre1 es ' + nombre1 );
+    alert('El nombre2 es ' + nombre2 );
+    alert('El nombre3 es ' + nombre3 );
+
+} */
+
+/* equipoDeFutbol('alvaro', 'pedro', 'juan') */
+
+function lavarseLosDientes(seLavo){ // definicion
+
+    if(seLavo){
+        return 'Se lavo los dientes';
+    }else{
+        return 'No se lavo los dientes';
+    }
+}
+
+function desayunar(comida){
+
+    return 'La persona desayuno: '+comida;
+}
+
+function banarme(seBano){
+
+    if(seBano){
+        return 'Se bano';
+    }else{
+        return 'No se bano';
+    }
+}
+
+function ejecutarRutina(){
+
+    lavarseLosDientes(true); 
+    desayunar('Cereales');
+    banarme(false);
+
+
+}
+
+
+
+
+
+
+
---
+++ operadoresAritmeticos.js
@@ -0,0 +1,21 @@
+// + sumar
+// - restar
+// * multiplicar
+// / dividir
+// % resto
+// ++ incrementar
+// -- decrementar
+
+var x = 10
+var y = 5;
+var z = x + y; // 10 + 5
+var suma = 15 + 20;
+
+
+var a = 0;
+a++; // a pasaria a tener el valor de 1
+a--; // a pasaria a tener el valor de 0
+
+// resto se usa para comprobar si un numero es divisible por
+// otro con resto 0
+
